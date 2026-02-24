06ce433e4a5e35e4a0afa1889d91c3558ebdde44
Javohir-star <jmamadiyorov24@gmail.com>
2026-02-24 17:18:07 +0500

    javohir: added dining_table

---
+++ app/schemas/schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+class TableBase(BaseModel):
+    table_no: str
+    capacity: int
+    status: str
+
+class TableCreate(TableBase):
+    pass  # Admin CRUD uchun kerak bo‘lsa
+
+class TableRead(TableBase):
+    id: int
+    class Config:
+        orm_mode = True
